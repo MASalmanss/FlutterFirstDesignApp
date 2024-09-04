f904711ecd6dcd6f9ce8d11469773aa88a2a0da0
Mehmet Akif Salman <ahmetakifsalman@gmail.com>
2024-09-04 15:45:33 +0300

    First Page added

--- tasarim_calismasi/lib/anasayfa.dart
+++ tasarim_calismasi/lib/anasayfa.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tasarim_calismasi/renkler.dart';
 
@@ -48,13 +51,29 @@ class _AnasayfaState extends State<Anasayfa> {
                 ),
               ],
             ),
-            Column(
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text("20 dakika" , style: TextStyle(color: yazirenk2  , fontSize: 22 , fontWeight: FontWeight.bold),),
+                  Text("Delivery" , style: TextStyle(color: yazirenk2  , fontSize: 22),),
+                  Text("Meet lower , get ready to meet your beef" , style: TextStyle(color: yazirenk2  , fontSize: 22), textAlign: TextAlign.center,),
+                ],
+              ),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text("20 dakika" , style: TextStyle(color: yazirenk2  , fontSize: 22),),
-                Text("Delivery" , style: TextStyle(color: yazirenk2  , fontSize: 22),),
-                Text("Meet lower , get ready to meet your beef" , style: TextStyle(color: yazirenk2  , fontSize: 22),),
+                Text("5.98\$" , style: TextStyle(fontSize: 36 ,color: Anarenk , fontWeight: FontWeight.bold),),
+                SizedBox(width: 200,height: 50,
+                  child: TextButton(onPressed: (){},
+                    child: Text("Add to Cart" , style: TextStyle(color: Yazirenk , fontSize: 18),),
+                    style: TextButton.styleFrom(backgroundColor: Anarenk,
+                      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5)))
+                    ),
+                  ),
+                )
               ],
-            )
+            ),
 
           ],
         )
